12d41ca13f0c03249dbafdaf3b39ee9c4c615036
Elena Zub <e.zub@intetics.com>
2020-01-06 19:28:25 +0200

    Task 3 done

---
+++ Binding_Task3/Task.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binding_Task3
+{
+    public class Task
+    {
+        private static int counter = 1;
+
+        public int Id { get; }
+        public String Name { get; set; }
+
+        public String Description { get; set; }
+
+        public int Priority { get; set; }
+
+        public Task()
+        {
+            this.Id = counter++;
+        }
+    }
+}

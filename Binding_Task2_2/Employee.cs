b0ecaa8ca7a6c7f1ff8cdb37799f26146540c0cb
Elena Zub <e.zub@intetics.com>
2020-01-06 18:51:30 +0200

    Binding with DataContext and Static resource

---
+++ Binding_Task2_2/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binding_Task2_2
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public bool IsMaster { get; set; }
+    }
+}

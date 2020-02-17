43ded518e3281425d78903aa1472c7bb2f5ed88e
Elena Zub <e.zub@intetics.com>
2020-02-17 18:00:10 +0200

    Done

---
+++ Code_First/Models/Address.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Code_First.Models
+{
+    public class Address
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string City { get; set; }
+
+        public string Street { get; set; }
+
+        public string Zip { get; set; }
+    }
+}
---
+++ Code_First/Models/OrderStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Code_First.Models
+{
+    public class OrderStatus
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
+    }
+}

974b7037c256cae913717ce07b384e90d4d06a4e
Elena Zub <e.zub@intetics.com>
2020-03-19 00:39:11 +0200

    Some work

---
+++ DAL.Student/IStudentReporitory.cs
@@ -0,0 +1,18 @@
+﻿using DALStudent.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace DALStudent
+{
+    public interface IStudentRepository : IDisposable
+    {
+        IEnumerable<Student> GetStudents();
+        Student GetStudentById(int studentId);
+        void InsertStudent(Student student);
+        void DeleteStudent(int studentId);
+        void UpdateStudent(Student student);
+        void Save();
+    }
+}
---
+++ DAL.Student/UnitOfWork.cs
@@ -0,0 +1,76 @@
+﻿using DALStudent.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DALStudent
+{
+    public class UnitOfWork : IDisposable
+    {
+        private UniversityDbContext context = new UniversityDbContext();
+        private GenericRepository<Department> departmentRepository;
+        private GenericRepository<Course> courseRepository;
+        private GenericRepository<Student> studentRepository;
+
+        public GenericRepository<Department> DepartmentRepository
+        {
+            get
+            {
+                if (this.departmentRepository == null)
+                {
+                    this.departmentRepository = new GenericRepository<Department>(this.context);
+                }
+                return this.departmentRepository;
+            }
+        }
+
+        public GenericRepository<Course> CourseRepository
+        {
+            get
+            {
+                if (this.courseRepository == null)
+                {
+                    this.courseRepository = new GenericRepository<Course>(this.context);
+                }
+                return this.courseRepository;
+            }
+        }
+
+        public GenericRepository<Student> StudentRepository
+        {
+            get
+            {
+                if (this.studentRepository == null)
+                {
+                    this.studentRepository = new GenericRepository<Student>(this.context);
+                }
+                return this.studentRepository;
+            }
+        }
+
+        public void Save()
+        {
+            this.context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    this.context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
